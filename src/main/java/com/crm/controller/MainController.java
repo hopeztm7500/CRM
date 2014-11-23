e8fdff5eab23b73f09a34be1032942d655c7e2b7
Victor <hopeztm@163.com>
2014-11-23 08:30:49 +0800

    Framework
    First commit

---
+++ src/main/java/com/crm/controller/MainController.java
@@ -0,0 +1,57 @@
+package com.crm.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	public boolean checkAndAddAuth(ModelMap model){
+		Authentication auth = SecurityContextHolder.getContext()
+				.getAuthentication();
+		if (!auth.getPrincipal().equals("anonymousUser")) {
+			model.addAttribute("authenticated", true);
+			
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	@RequestMapping(value = { "/", "index" })
+	public String index(ModelMap model) {
+		checkAndAddAuth(model);
+	
+		return "index";
+	}
+
+	@RequestMapping(value = "product-details")
+	public String aboutUs(ModelMap model) {
+		checkAndAddAuth(model);
+		return "product-details";
+	}
+	
+
+	@RequestMapping(value = "cart")
+	public String cart(ModelMap model) {
+		checkAndAddAuth(model);
+		return "cart";
+	}
+
+	@RequestMapping(value = "contact-us")
+	public String contactUs(ModelMap model) {
+		checkAndAddAuth(model);
+		return "contact-us";
+	}
+
+}
