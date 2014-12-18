ba12d871f80dff8153e756866ae1f8a357254b18
Victor <hopeztm@163.com>
2014-12-18 08:51:47 +0800

    add fav sku

--- src/main/webapp/assets/js/pages/member-detail-controller.js
+++ src/main/webapp/assets/js/pages/member-detail-controller.js
@@ -14,6 +14,9 @@ module.controller('MemberDetailController', function($scope, $http) {
 	}
 	
 	function drawFavProductChart(data){
+		if(!data){
+			return ;
+		}
 		require.config({
 			paths : {
 				echarts : 'http://echarts.baidu.com/build/dist'
@@ -21,22 +24,31 @@ module.controller('MemberDetailController', function($scope, $http) {
 		});
 
 		var legend = [];
-		var series = [];
+		var mydata = {
+				name:"次数",
+				type:"bar",
+				data:[]
+		};
+		var xlengend = [];
 		var map = {};
+		_.each(data, function(v,i){
+			xlengend.push(v.skuDto.skuName);
+			mydata.data.push(v.times);
+		});
 		
 		require([ 'echarts', 'echarts/chart/bar' ], function(ec) {
 			var myChart = ec.init(document.getElementById('bar-chart'));
 
 			option = {
 				    title : {
-				        text: '某地区蒸发量和降水量',
-				        subtext: '纯属虚构'
+				        text: '产品购买次数',
+				        
 				    },
 				    tooltip : {
 				        trigger: 'axis'
 				    },
 				    legend: {
-				        data:['蒸发量','降水量']
+				        data:['次数']
 				    },
 				    toolbox: {
 				        show : true,
@@ -52,7 +64,7 @@ module.controller('MemberDetailController', function($scope, $http) {
 				    xAxis : [
 				        {
 				            type : 'category',
-				            data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+				            data : xlengend
 				        }
 				    ],
 				    yAxis : [
@@ -60,40 +72,7 @@ module.controller('MemberDetailController', function($scope, $http) {
 				            type : 'value'
 				        }
 				    ],
-				    series : [
-				        {
-				            name:'蒸发量',
-				            type:'bar',
-				            data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
-				            markPoint : {
-				                data : [
-				                    {type : 'max', name: '最大值'},
-				                    {type : 'min', name: '最小值'}
-				                ]
-				            },
-				            markLine : {
-				                data : [
-				                    {type : 'average', name: '平均值'}
-				                ]
-				            }
-				        },
-				        {
-				            name:'降水量',
-				            type:'bar',
-				            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
-				            markPoint : {
-				                data : [
-				                    {name : '年最高', value : 182.2, xAxis: 7, yAxis: 183, symbolSize:18},
-				                    {name : '年最低', value : 2.3, xAxis: 11, yAxis: 3}
-				                ]
-				            },
-				            markLine : {
-				                data : [
-				                    {type : 'average', name : '平均值'}
-				                ]
-				            }
-				        }
-				    ]
+				    series : [mydata]
 			};
 
 			myChart.setOption(option);
@@ -106,7 +85,7 @@ module.controller('MemberDetailController', function($scope, $http) {
 	    $scope.favoriteSkus = data.skus;
 	    $scope.rfm = data.rfm;
 	    
-	    drawFavProductChart(data.favoriteSkus);
+	    drawFavProductChart($scope.favoriteSkus);
 	    
 	});
 	
